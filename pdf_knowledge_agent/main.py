4acb1255e2f1e83a76554b071d458c9ca8fb79ab
Caio Machado <moliveiracaio@gmail.com>
2025-03-18 11:40:38 -0300

    creating PDF searcher agent

---
+++ pdf_knowledge_agent/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import warnings
+from crew import QuestKnowledge
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        "question": "How often should I take breaks?",
+    }
+    QuestKnowledge().crew().kickoff(inputs=inputs)
+
+
+if __name__ == "__main__":
+    run()
